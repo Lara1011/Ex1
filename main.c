137cf56e9f6520bfdbca3a991fc639fba5346798
Lara1011 <76583122+Lara1011@users.noreply.github.com>
2021-12-01 23:52:10 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,8 @@
 #include <math.h>
 
 int main() {
-    printf("Enter your first number: ");
     int num1;
     scanf("%d", &num1);
-      printf("Enter your second number: ");
     int num2;
     scanf("%d", &num2);
     int maxNum = num2;
@@ -16,25 +14,25 @@ int main() {
         maxNum = num1;
         minNum = num2;
     }
-    printf("The Armstrong numbers between %d - %d are : ", minNum,maxNum);
+    printf("The Armstrong numbers are: ", minNum,maxNum);
     for(int i= minNum ; i<=maxNum ; i++){
         if(isArmstrong(i)==1){
             printf("%d ", i);
         }
     }
-      printf("\nThe Palindrome numbers between %d - %d are : ", minNum,maxNum);
+      printf("\nThe Palindromes are: ", minNum,maxNum);
     for(int i= minNum ; i<=maxNum ; i++){
         if(isPalindrome(i)==1){
             printf("%d ", i);
         }
     }
-      printf("\nThe Prime numbers between %d - %d are : ", minNum,maxNum);
+      printf("\nThe Prime numbers are: ", minNum,maxNum);
     for(int i= minNum ; i<=maxNum ; i++){
         if(isPrime(i)==1){
             printf("%d ", i);
         }
     }
-      printf("\nThe Strong numbers between %d - %d are : ", minNum,maxNum);
+      printf("\nThe Strong numbers are: ", minNum,maxNum);
     for(int i= minNum ; i<=maxNum ; i++){
         if(isStrong(i)==1){
             printf("%d ", i);
